38340578d2a2d0559a800eafcdc06a63dc4bb583
jb <jacopo.bonato@iit.it>
2020-01-29 12:50:40 +0100

    setup2 ok

--- installation/set_up2.sh
+++ installation/set_up2.sh
@@ -9,6 +9,22 @@ cd $PARDIR
 
 virtualenv .$ENV_NAME
 ENV_PATH="."$ENV_NAME"/bin/activate"
-#echo $ENV_PATH
+##echo $ENV_PATH
+echo 'export PATH=/usr/local/cuda-10.1/bin:$PATH:' >> $ENV_PATH
+echo 'export CUDADIR=/usr/local/cuda-10.1' >> $ENV_PATH
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64:$LD_LIBRARY_PATH' >> $ENV_PATH
 source $ENV_PATH
-
+#
+##check cuda installation comment if it is not necessary
+cp -a /usr/local/cuda-10.1/samples samples-10.1
+cd samples-10.1
+make -j 4
+cd ..
+########
+## installation of python packages
+#
+pip install -r requirements.txt
+########
+echo Installation complete
+echo type source $ENV_PATH to activate the environment
+echo type deactivate to exit from the environment
