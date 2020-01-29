ad86805282388a6ce7559624ec077b178c93cd8a
jbonato <jacopo.bonato@iit.it>
2020-01-29 15:54:45 +0100

    installer test

--- installation/gpu_setup1.sh
+++ installation/gpu_setup1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
-#if [ "$EUID" -ne 0 ]; then
-#	echo "Please run as root (with sudo)"
-#	exit
-#fi
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run as root (with sudo)"
+	exit
+fi
 
-SETUP_DIR="$PWD"
-echo SETUP_DIR
+#SETUP_DIR=$PWD
+#echo $SETUP_DIR
 #mkdir -p $SETUP_DIR
 #cd $SETUP_DIR
 
@@ -17,10 +17,10 @@ echo SETUP_DIR
 if [ ! -f "cuda_10.1.105_418.39_linux.run" ]; then 
 	wget https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.105_418.39_linux.run 
 fi
-#chmod +x cuda_10.1.105_418.39_linux.run
-#sudo sh cuda_10.1.105_418.39_linux.run --silent --toolkit --toolkitpath=/usr/local/cuda-10.1
+chmod +x cuda_10.1.105_418.39_linux.run
+sudo sh cuda_10.1.105_418.39_linux.run --silent --toolkit --toolkitpath=/usr/local/cuda-10.1
 #
 echo "Setup requires a reboot to continue."
-echo "The VM will reboot now. Login after it restarts and continue installation from part2."
+echo "The WS/VM will reboot now. Login after it restarts and continue installation from part2."
 
-#sudo reboot
+sudo reboot
--- installation/set_up2.sh
+++ installation/set_up2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ENV_NAME="AstroSSS"
+ENV_NAME="envTest"
 
 echo $ENV_NAME
 STDIR=$PWD
 PARDIR="$(dirname "$PWD")"
 cd $PARDIR
 
-virtualenv .$ENV_NAME
+virtualenv --python=python3.6 .$ENV_NAME
 ENV_PATH="."$ENV_NAME"/bin/activate"
 ##echo $ENV_PATH
 echo 'export PATH=/usr/local/cuda-10.1/bin:$PATH:' >> $ENV_PATH
@@ -16,10 +16,10 @@ echo 'export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64:$LD_LIBRARY_PATH' >> $EN
 source $ENV_PATH
 #
 ##check cuda installation comment if it is not necessary
-cp -a /usr/local/cuda-10.1/samples samples-10.1
-cd samples-10.1
-make -j 4
-cd ..
+#cp -a /usr/local/cuda-10.1/samples samples-10.1
+#cd samples-10.1
+#make -j 4
+#cd ..
 ########
 ## installation of python packages
 #
